f1d37ed28e51142fb6502abf270d94b01d6d00e2
Caner Türkmen <canerturkmen@msn.com>
2023-04-25 23:28:30 +0300

    getStaticProps replaced with useEffect

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,9 +1,32 @@
 import { MapWithBars } from "@/components/Map/MapWithBars";
+import { getMarkers } from "@/lib/get-markers";
+import { MarkerDataType } from "@/types";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function Map() {
+  const [markers, setMarkers] = useState<MarkerDataType[]>([]);
+
+  // Fetch markers from API
+  // Temp solution for now, will be replaced with getStaticProps
+  useEffect(() => {
+    async function getMarkers() {
+      const markers = await fetch("/api/markers").then((res) => res.json());
+      setMarkers(markers);
+    }
+    getMarkers();
+  }, []);
+
   return (
     <>
       <MapWithBars />
     </>
   );
 }
+
+// export async function getStaticProps() {
+//   const markers = await getMarkers();
+//   return {
+//     props: { markers },
+//     revalidate: 1,
+//   };
+// }
